ce6543b39de654b189887aa4f77d965c7f52829e
IlahaLatifova <116644225+IlahaLatifova@users.noreply.github.com>
2022-12-04 23:05:29 +0400

    Commit

---
+++ ConsoleApp1/Optional.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Optional<T>
+    {
+        T[] values = new T[0];
+
+        public T this[int index]
+        {
+            get => values[index];
+            set => values[index] = value;
+        }
+
+        public int Length()
+        {
+            return values.Length;
+        }
+
+        public void Add(T value)
+        {
+            Array.Resize(ref values, values.Length + 1);
+            values[values.Length - 1] = value;
+        }
+
+        public bool Remove(T value)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values.Contains(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int IndexOf(T value)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i].Equals(value))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int LastIndexOf(T value)
+        {
+            for (int i = values.Length - 1; i >= 0; i--)
+            {
+                if (values[i].Equals(value))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool Contains(T value)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //public void Reverse()
+        //{
+        //    for (int i = 0, j = values.Length - 1; i > values.Length; i++, j--)
+        //    {
+        //        T value = values[i];
+        //        values[i] = values[j];
+        //        values[j] = value;
+        //    }
+        //}
+
+        public void Clear()
+        {
+            Array.Resize(ref values, 0);
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            List<string> list = new List<string>();
+            list.Add("amama");
+            list.Add("ananas");
+            list.Add("apelsin");
+            list.Add("alma");
+
+            List<int> list2 = new List<int>();
+            list2.Add(9);
+            list2.Add(3);
+            list2.Add(6);
+            list2.Add(4);
+
+            //Console.WriteLine(list.Remove("amama"));
+            //Console.WriteLine(list.IndexOf("amama"));
+            //Console.WriteLine(list.LastIndexOf("ananas"));
+            //Console.WriteLine(list.Contains("apelsin"));
+            //list2.Reverse();
+            //foreach (var item in list2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //list2.Clear();
+            //foreach (var item in list2)
+            //{
+            //    Console.WriteLine(item);
+            //}*/
+
+            Optional<string> optional = new Optional<string>();
+            optional.Add("amama");
+            optional.Add("ananas");
+            optional.Add("apelsin");
+            optional.Add("alma");
+
+            Optional<int> optional2 = new Optional<int>();
+            optional2.Add(7);
+            optional2.Add(9);
+            optional2.Add(3);
+            optional2.Add(5);
+
+            //Console.WriteLine(optional.Remove("amama"));
+            //Console.WriteLine(optional.IndexOf("amama"));
+            //Console.WriteLine(optional.LastIndexOf("ananas"));
+            //Console.WriteLine(optional.Contains("apelsin"));
+
+            //optional2.Reverse();
+            //for (int i = 0; i < optional2.Length(); i++)
+            //{
+            //    Console.WriteLine(optional2[i]);
+            //}
+
+            optional2.Clear();
+            Console.WriteLine(optional2.Length());
+
+
+        }
+    }
+}
